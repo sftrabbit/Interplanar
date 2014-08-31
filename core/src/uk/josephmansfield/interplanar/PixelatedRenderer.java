92bc0e07e8ff3b615bce3e96cbb70a604619892e
Joseph Mansfield <sftrabbit@gmail.com>
2014-08-31 19:53:31 +0100

    Add camera to scene renderer

--- core/src/uk/josephmansfield/interplanar/PixelatedRenderer.java
+++ core/src/uk/josephmansfield/interplanar/PixelatedRenderer.java
@@ -37,7 +37,7 @@ public class PixelatedRenderer implements Renderer {
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         renderBuffer.begin();
--- core/src/uk/josephmansfield/interplanar/SceneRenderer.java
+++ core/src/uk/josephmansfield/interplanar/SceneRenderer.java
@@ -1,15 +1,19 @@
 package uk.josephmansfield.interplanar;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class SceneRenderer implements Renderer {
+
+    private Camera camera = new OrthographicCamera();
+    private ShapeRenderer shapeRenderer = new ShapeRenderer();
+    private Viewport viewport = null;
 
     private int width = 0;
     private int height = 0;
@@ -21,12 +25,23 @@ public class SceneRenderer implements Renderer {
     public void resize(int width, int height) {
         this.width = width;
         this.height = height;
+
+        if (viewport == null) {
+            viewport = new FitViewport(width, height, camera);
+        }
     }
 
     @Override
     public void render() {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        viewport.update(width, height);
+
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setProjectionMatrix(camera.combined);
+        shapeRenderer.line(0, 0, 5f, 5f);
+        shapeRenderer.end();
     }
 
     @Override
