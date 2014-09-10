004fff4f2058610b52191620485bd1c0af3a2d72
Joseph Mansfield <sftrabbit@gmail.com>
2014-09-10 21:36:49 +0100

    Extract PhysicsComponent details out

--- core/src/uk/josephmansfield/interplanar/entities/CharacterEntity.java
+++ core/src/uk/josephmansfield/interplanar/entities/CharacterEntity.java
@@ -3,14 +3,27 @@ package uk.josephmansfield.interplanar.entities;
 
 import com.badlogic.ashley.core.Component;
 import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import uk.josephmansfield.interplanar.entities.components.PhysicsComponent;
 
 public class CharacterEntity extends Entity {
 
 	public CharacterEntity(World physicsWorld) {
-		PhysicsComponent physicsComponent = new PhysicsComponent();
+		Vector2 position = new Vector2();
+
+		PolygonShape square = new PolygonShape();
+		square.setAsBox(8f, 8f);
+
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = square;
+
+		PhysicsComponent physicsComponent = new PhysicsComponent(position, fixtureDef);
 		physicsComponent.addToPhysicsWorld(physicsWorld);
 		add(physicsComponent);
+
+		square.dispose();
 	}
 }
--- core/src/uk/josephmansfield/interplanar/entities/components/PhysicsComponent.java
+++ core/src/uk/josephmansfield/interplanar/entities/components/PhysicsComponent.java
@@ -1,29 +1,36 @@
 package uk.josephmansfield.interplanar.entities.components;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
+
+import java.util.ArrayList;
 
 public class PhysicsComponent extends Component {
 
-	public PhysicsComponent() {
+	private Vector2 position;
+	private ArrayList<FixtureDef> fixtureDefs = new ArrayList<FixtureDef>();
+
+	public PhysicsComponent(Vector2 position, ArrayList<FixtureDef> fixtureDefs) {
+		this.position = position;
+		this.fixtureDefs.addAll(fixtureDefs);
+	}
+
+	public PhysicsComponent(Vector2 position, FixtureDef fixtureDef) {
+		this.position = position;
+		this.fixtureDefs.add(fixtureDef);
 	}
 
 	public void addToPhysicsWorld(World physicsWorld) {
 		BodyDef bodyDef = new BodyDef();
 		bodyDef.type = BodyDef.BodyType.DynamicBody;
-		bodyDef.position.set(0f, 0f);
+		bodyDef.position.set(position);
 
 		Body body = physicsWorld.createBody(bodyDef);
 
-		PolygonShape square = new PolygonShape();
-		square.setAsBox(8f, 8f);
-
-		FixtureDef fixtureDef = new FixtureDef();
-		fixtureDef.shape = square;
-
-		Fixture fixture = body.createFixture(fixtureDef);
-
-		square.dispose();
+		for (FixtureDef fixtureDef : fixtureDefs) {
+			body.createFixture(fixtureDef);
+		}
 	}
 
 }
